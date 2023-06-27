a59479b7bdcb38c0606b1e4bea375aa49e7593fd
Chritelle-12 <mariechristellenirere@gmail.com>
2023-06-27 09:56:58 +0200

    Add the linters configuration

--- student.rb
+++ student.rb
@@ -1,10 +1,11 @@
-require "./person.rb"
+require './person'
 class Student < Person
-    def initialize(classroom, age, name ="unkown", parent_permission = true)
-        super(age, name, parent_permission)
-        @classroom =classroom
-    end 
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
+  def initialize(classroom, age, name = 'unkown', parent_permission = true)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯(ツ)/¯'
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,13 +1,14 @@
-require "./person.rb"
+require './person'
 
 class Teacher < Person
-    def initialize(specialization,age, name ="unkown", parent_permission = true)
-        super(age,name,parent_permission)
-        @specialization =specialization
-    end
-    def can_use_services?
-        true
-    end
+  def initialize(specialization, age, name = 'unkown', parent_permission = true)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
 end
-teacher = Teacher.new("Math", 10, "lucas")
-puts teacher.can_use_services? 
+teacher = Teacher.new('Math', 10, 'lucas')
+puts teacher.can_use_services?
